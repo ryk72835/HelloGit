37300b03f8f9417827a79ca232f003822b3cd06a
ryk <728357137@qq.com>
2021-03-02 21:50:11 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/itryk/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.itryk.git;
+
+/**
+ * @author ryk
+ * @version 1.0.0
+ * @ClassName HelloGit.java
+ * @Description TODO
+ * @createTime 2021年03月01日 23:26:00
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
